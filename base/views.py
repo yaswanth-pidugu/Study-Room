41b68978160ee4ff70a5e7029cd195df91f12483
Yashwanth Roy <yashwanthpidugu999@gmail.com>
2025-09-27 18:26:47 +0530

    Initial commit

--- base/views.py
+++ base/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 # Create your views here.
-rooms = [
+"""rooms = [
     {'id' : 1, 'name' : 'Lets learn python!'},
     {'id' : 2, 'name' : 'Design with me'},
     {'id' : 3, 'name' : 'Frontend Developers!'},
 ]
-
+"""
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,"base/home.html",context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        context = {'room':room}
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
     return render(request,"base/room.html",context)
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form' : form}
+    return render(request,"base/room_form.html",context)
