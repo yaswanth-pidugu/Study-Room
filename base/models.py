5a72422ec0b30971b2c2a7d53268bdf6ce00aa3d
Yashwanth Roy <yashwanthpidugu999@gmail.com>
2025-10-21 18:22:02 +0530

    Updated

--- base/models.py
+++ base/models.py
@@ -1,19 +1,59 @@
 from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=False)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True, default="avatar.svg")
+
+    username = None  # Remove the username field
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # No extra fields required
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+
 class Topic(models.Model):
     name = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return self.name
-    
+
 
 class Room(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL,null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL,null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #participants=
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -22,13 +62,17 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.body[0:50]
